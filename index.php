5b6d7247d744f285d19ce8dae83e0abf71b3caac
Sandeep Kapoor <sankapoo@redhat.com>
2025-05-15 13:10:08 +0530

    date added

--- index.php
+++ index.php
@@ -5,7 +5,12 @@ echo "<h1 style='color:#d9534f;font-family:Arial,Helvetica,sans-serif'>
 echo "<h2 style='color:#0275d8;font-weight:bold'>
         Greetings from Punjab National Bank
       </h2>";
+
 echo "<p style='font-size:18px'>
         We’re delighted to host you  🚀
       </p>";
+
+echo "<h2 style='color:#0275d8;font-weight:bold'>
+        Dated 15th May, 2025
+      </h2>";
 
